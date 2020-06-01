24cfbf0e052692b87030c1027c940b36487c11c8
Henry Dlhopolsky <henrydl@umich.edu>
2020-05-31 20:23:51 -0400

    wrote basic client

--- src/persistence/journal.ts
+++ src/persistence/journal.ts
@@ -1,3 +1,23 @@
-export function getActivities( ) {
-	return [];
+const axios = require('axios');
+
+//put
+export function putActivity(activity_type) {
+	let url = "http://localhost:3000/activities/";
+	let post_body.activity_type = activity_type;
+	let post_body.start_time = new Date();
+	return axios.put(url, post_body).catch(e => console.log(e));;
+}
+
+//get schedule up to now for today
+export function getScheduleUpToNow() {
+	let url = "http://localhost:3000/activities/";
+	let params.end_time = new Date();
+	let parmas.start_time = new Date().setHours(0,0,0,0);
+	return axios.get(url, params).catch(e => console.log(e));;
+}
+
+//get current activity
+export function getCurrentActivity() {
+	let url = "http://localhost:3000/activities/current";
+	return axios.get(url).catch(e => console.log(e));
 }
